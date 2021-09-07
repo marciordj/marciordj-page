59280b3e24273854607fa9d30b6e767f0d459262
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-09-07 13:07:59 -0300

    feature: add header

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import style from './style.module.scss';
+
 const Header = () => {
   return (
-    <div>
-      <h1>Hello Headerrr</h1>
+    <div className={style.container}>
+      <div className={style.headerContent}>
+        <h2>marciordj</h2>
+
+        <nav className={style.sections}>
+          <p>Home</p>
+          <p>Post</p>
+        </nav>
+      </div>
     </div>
   )
 }
